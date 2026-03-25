b1ebf64e72248ef33306e66c65e2c1b2e6cef83e
GABRIEL EDUARDO <gabriel_eduardo150@estudante.sesisenai.org.br>
2026-03-24 21:40:05 -0300

    first commit

---
+++ react-menu-ts-app/src/pages/DisciplinaPage.tsx
@@ -0,0 +1,32 @@
+export default function PerfilPage() {
+  return (
+    <section>
+      <h1>Disciplinas Senai</h1>
+      <div className="profile-box">
+        <p><strong>Nome:</strong> Banco de Dados</p>
+        <p><strong>Professor:</strong> Sérgio</p>
+        <p><strong>Tecnologias:</strong> PostgreSQL</p>
+      </div>
+      <div className="profile-box">
+        <p><strong>Nome:</strong> Lógica de Programação</p>
+        <p><strong>Professor:</strong> Sérgio</p>
+        <p><strong>Tecnologias:</strong> Portugol, Javascript</p>
+      </div>
+      <div className="profile-box">
+        <p><strong>Nome:</strong> Aplicativos</p>
+        <p><strong>Professor:</strong> Oscar</p>
+        <p><strong>Tecnologias:</strong> React</p>
+      </div>
+      <div className="profile-box">
+        <p><strong>Nome:</strong> Desenvolvimento de Sistemas</p>
+        <p><strong>Professor:</strong> Natan, Marcelo</p>
+        <p><strong>Tecnologias:</strong> Javascript, Node, React, Typescript</p>
+      </div>
+      <div className="profile-box">
+        <p><strong>Nome:</strong> Teste de Sistemas</p>
+        <p><strong>Professor:</strong> Rafael</p>
+        <p><strong>Tecnologias:</strong> Javascript</p>
+      </div>
+    </section>
+  )
+}
